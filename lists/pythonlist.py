b9e377a2cfbd0156166fd6afc74874721bee7f9d
Md Mamun <mamun@smartfieldservice.com>
2024-10-02 17:55:23 +0600

    practice list

--- lists/pythonlist.py
+++ lists/pythonlist.py
@@ -1,5 +1,18 @@
 #Simple array
 thislist = ["apple","banana","cherry"]
-print(thislist[0])
+
+#List can be changed 
+thislist[0] = "Coconat"
+
+# python list add item using append() method
+thislist.append("angur")
+
+# or we can add item as index wise using insert(index, "value")
+thislist.insert(3, "angur")
+
+#python removed item from lists
+thislist.remove("banana")
+
+print(thislist)
 
 # array length

a8255a863cb269d65fbd488b753010b46aa47114
Md Mamun <mamun@smartfieldservice.com>
2025-03-11 12:35:18 +0600

    stack and stack operations add, remove and peek in python

---
+++ ostad-dsa-practice/module-8(stack & queue)/concepts/stack.py
@@ -0,0 +1,11 @@
+# stack is LIFO based data structure 
+# stack operations are push for add, pop for removed and peek for view top element 
+
+list = []
+list.append(1)
+list.append(2)
+list.append(3)
+print(list)
+print(list[len(list)-1])
+list.remove()
+print(list)
